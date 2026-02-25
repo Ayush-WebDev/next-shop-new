6cb8bca28ca723325c0fcc5f87fcd1ee9f42991d
ayush kumar <ayuk451998@gmail.com>
2026-02-25 14:54:13 +0530

    added not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section from "@/components/global/Section";
+import SectionTitle from "@/components/global/SectionTitle";
+const NotFound = () => {
+  return (
+    <Section>
+      <SectionTitle text="404 Not found"></SectionTitle>
+    </Section>
+  );
+};
+
+export default NotFound;
